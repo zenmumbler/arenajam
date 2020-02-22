feae3542687068a55cbaaccba9f79c7da4d5a812
Tobias Guse <tobias.guse@mogul.gg>
2020-02-22 20:18:22 +0100

    created starter map

---
+++ source-assets/Male 12-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Male 12-1" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="Male/Male 12-1.png" width="96" height="128"/>
+</tileset>
---
+++ source-assets/hyptosis_tile-art-batch-3-transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="hyptosis_tile-art-batch-3" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="hyptosis_tile-art-batch-3.png" trans="232323" width="960" height="960"/>
+</tileset>
