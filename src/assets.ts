d515d562410f1d00cc4adc7b6b430c295a190edc
Arthur Langereis <puurtuur@me.com>
2020-02-22 22:47:48 +0100

    asset loading work for Tiled files and spritesheets

---
+++ src/assets.ts
@@ -0,0 +1,52 @@
+export function loadImage(url: string) {
+	return new Promise<HTMLImageElement>((resolve, reject) => {
+		const image = new Image();
+		image.onload = function() {
+			resolve(image);
+		};
+		image.onerror = function() {
+			reject(`Could not load image at ${url}`);
+		};
+		image.src = url;
+	});
+}
+
+export async function loadImageData(fileName: string) {
+	const image = await loadImage(fileName);
+	const canvas = document.createElement("canvas");
+	canvas.width = image.width;
+	canvas.height = image.height;
+	const ctx = canvas.getContext("2d")!;
+	ctx.imageSmoothingEnabled = false;
+	ctx.drawImage(image, 0, 0);
+	return ctx.getImageData(0, 0, image.width, image.height);
+}
+
+export interface SpriteSheet {
+	tileDim: number;
+	columns: number;
+	rows: number;
+	image: CanvasImageSource;
+}
+
+export async function loadSpriteSheet(fileName: string, tileDim: number): Promise<SpriteSheet> {
+	const image = await loadImage(fileName);
+	return {
+		tileDim,
+		columns: (image.width / tileDim) | 0,
+		rows: (image.height / tileDim) | 0,
+		image
+	};
+}
+
+export async function loadXMLDocument(url: string) {
+	const response = await fetch(url);
+	const text = await response.text();
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(text, "application/xml");
+	return doc.firstElementChild!;
+}
+
+export function resolveRelativeURL(base: string, rel: string) {
+	return (new URL(rel, base)).href;
+}
---
+++ src/tilemap.ts
@@ -0,0 +1,176 @@
+// Minimal support for orthogonal right-down order, base64 encoded tile data
+
+import { loadXMLDocument, SpriteSheet, loadSpriteSheet, resolveRelativeURL } from "./assets";
+
+function stringAttr(el: Element, name: string, def = "") {
+	return el.attributes.getNamedItem(name)?.textContent ?? def;
+}
+
+function intAttr(el: Element, name: string, def = 0) {
+	const str = stringAttr(el, name);
+	return str.length ? parseInt(str, 10) : def;
+}
+
+export const enum TileRot {
+	None = 0,
+	Diag = 1,
+	Vert = 2,
+	Horiz = 4
+}
+
+export interface TMXLayer {
+	name: string;
+	width: number;
+	height: number;
+	properties: Record<string, string>;
+	tileRotations: Uint8Array;
+	tileIDs: Uint32Array;
+}
+
+/*
+class TMXLayer {
+	tileAt(col: number, row: number) {
+		if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
+			return -1;
+		}
+		return this.tileIDs[(row * this.width) + col];
+	}
+
+	setTileAt(col: number, row: number, tile: number) {
+		if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
+			return;
+		}
+		this.tileIDs[(row * this.width) + col] = tile;
+	}
+
+	eachTile(callback: (row: number, col: number, tile: number, rot: TileRot) => void) {
+		let off = 0;
+		for (let row = 0; row < this.height; ++row) {
+			for (let col = 0; col < this.width; ++col) {
+				if (this.tileIDs[off]) {
+					callback(row, col, this.tileIDs[off], this.tileRotations[off]);
+				}
+				++off;
+			}
+		}
+	}
+}
+*/
+
+function loadLayer(layerEl: Element): TMXLayer {
+	const width = intAttr(layerEl, "width");
+	const height = intAttr(layerEl, "height");
+	const name = stringAttr(layerEl, "name");
+	const properties: Record<string, string> = {};
+	let tileIDs: Uint32Array | undefined;
+	let tileRotations: Uint8Array | undefined;
+
+	layerEl.childNodes.forEach(node => {
+		const el = node as Element;
+		if (el.nodeName === "properties") {
+			el.childNodes.forEach(prop => {
+				const propEl = prop as Element;
+				const propName = stringAttr(propEl, "name");
+				const propVal = stringAttr(propEl, "value");
+				properties[propName] = propVal;
+			});
+		}
+		else if (el.nodeName === "data") {
+			const dataStr = el.textContent || "";
+			const enc = stringAttr(el, "encoding");
+			if (enc === "base64") {
+				const byteView = new Uint8Array(atob(dataStr).split("").map(c => { return c.charCodeAt(0); }));
+				tileIDs = new Uint32Array(byteView.buffer);
+			}
+			else if (enc === "csv") {
+				tileIDs = new Uint32Array(dataStr.split(",").map(s => parseInt(s.trim())));
+			}
+			else {
+				throw new Error(`unknown layer data encoding: ${enc}`);
+			}
+
+			// extract rotation data into separate array, clearing the meta bits off of the tileID
+			tileRotations = new Uint8Array(tileIDs.length);
+			for (let off = 0; off < tileIDs.length; ++off) {
+				const tile = tileIDs[off];
+				tileRotations[off] = tile >>> 28;
+				tileIDs[off] = tile & 0x1FFF_FFFF;
+			}
+		}
+	});
+
+	if (tileIDs && tileRotations) {
+		return {
+			width,
+			height,
+			name,
+			properties,
+			tileIDs,
+			tileRotations
+		};
+	}
+	else {
+		throw new Error("No layer data present in layer");
+	}
+}
+
+export interface TileSet extends SpriteSheet {
+	name: string;
+	firstGID: number;
+}
+
+async function loadTileSet(def: Element): Promise<TileSet> {
+	const firstGID = intAttr(def, "firstgid");
+	const source = stringAttr(def, "source");
+	if (source) {
+		def = await loadXMLDocument(resolveRelativeURL("./", source));
+	}
+	const tileDim = intAttr(def, "tilewidth");
+
+	const image = def.firstElementChild;
+	if (image && image.nodeName === "image") {
+		const imageURL = stringAttr(image, "source");
+		const sheet = await loadSpriteSheet(imageURL, tileDim);
+		return {
+			...sheet,
+			firstGID,
+			name
+		};
+	}
+	else {
+		throw new Error("expected image as first child of tilset");
+	}
+}
+
+export interface TMXMap {
+	layers: TMXLayer[];
+	tileSets: TileSet[];
+	width: number;
+	height: number;
+}
+
+export async function loadTMXMap(url: string): Promise<TMXMap> {
+	const tileDoc = await loadXMLDocument(url);
+
+	const width = intAttr(tileDoc, "width");
+	const height = intAttr(tileDoc, "height");
+	const layers: TMXLayer[] = [];
+	const tileSetLoads: Promise<TileSet>[] = [];
+
+	tileDoc.childNodes.forEach(node => {
+		const el = node as Element;
+		if (node.nodeName === "tileset") {
+			tileSetLoads.push(loadTileSet(el));
+		}
+		if (node.nodeName === "layer") {
+			layers.push(loadLayer(el));
+		}
+	});
+
+	return {
+		width,
+		height,
+		layers,
+		tileSets: await Promise.all(tileSetLoads)
+	};
+}
